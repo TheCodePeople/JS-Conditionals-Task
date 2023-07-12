c959d551dc741ec25bb7d8f60ebb74773f0822a2
3lawi <alii.3mmar@gmail.com>
2023-07-12 17:35:38 +0300

    Complete IfStatement.js Task

--- ifStatement.js
+++ ifStatement.js
@@ -13,19 +13,29 @@ Note:
    You can change the value of the age variable to test different cases. If the person's age is less than the minimum driving age, nothing will be printed.
 ************************************************************************************************/
 // TODO: ADD YOUR CODE BELOW
+let userAge = 22;
+let minimumDrivingAge = 18;
+
+
+if (userAge >= minimumDrivingAge) {
+  console.log("You are old enough to drive");
+}else{
+  console.log("You are not eligible to get a license yet");
+}
+
 
 /************************************************************************************************
-Task 2 (if..else Statement): 
+Task 2 (if..else Statement):
   Create a program that checks if a person is an admin, using just if...else statement.
 
-Description: 
+Description:
   In this task, you will create a program that checks if a person is an admin, using just if...else statement.
   You will define two variables, userName and role, and assign them the values userName and Admin respectively.
   Then, you will check if the person's userName is equal to userName and their role is equal to Admin.
   If both conditions are true, you will print a message saying that the person has permission to enter.
   Otherwise, you will print a message saying that they are not allowed to enter.
 
-Steps:  
+Steps:
   1. Define two variables username and role, and assign them the values "YOUR_NAME" and "Admin" respectively.
   2. Ask the user to enter his/her name and store it in a variable called enteredUsername.
   3. Check if the entered username is equal to YOUR_NAME and the role is equal to Admin.
@@ -33,6 +43,17 @@ Steps:
     - If either of the conditions is false, print the message Hello ${userName}, I'm sorry but it seems you're not authorized to access the restricted area.
 ***********************************************************/
 // TODO: ADD YOUR CODE BELOW
+let userName = "ali3mmar";
+let role = "Admin";
+let enteredUsername = prompt("Enter your name");
+
+
+if (enteredUsername == userName && role == "Admin") {
+  console.log(`Hello ${userName}, and you have permission to access the restricted area`);
+}else{
+  console.log(`Hello ${userName}, I'm sorry but it seems you're not authorized to access the restricted area`);
+}
+
 
 /************************************************************************************************
 Task 3 (if..else Statement):
@@ -47,21 +68,48 @@ Steps:
     - If the number is zero, print a message saying that it is zero.
 ************************************************************************************************/
 // TODO: ADD YOUR CODE BELOW
+let number = prompt("Enter a number");
+number = parseInt(number);
+
+
+if (number > 0) {
+  console.log("The number is positive");
+}else if (number < 0) {
+  console.log("The number is negative");
+}else{
+  console.log("The number is zero");
+}
 
 /*************************************************************************************************
 Task 4 (if..else Statement):
 
 Steps:
   1. Create a program that prompts the user for their age and checks if they are old enough to vote.
-    - If the user is 18 or older, the program should ask if they are registered to vote (Hint: Use confirm() function). 
+    - If the user is 18 or older, the program should ask if they are registered to vote (Hint: Use confirm() function).
     - If they are registered, the program should print the message "Great! You are all set to vote."
     - If they are not registered, the program should print the message "You still have time to register to vote."
     - If the user is younger than 18, the program should calculate how many years are left until they turn 18 and print the message "You will be eligible to vote in X years", where X is the number of years left.
 ************************************************************************************************/
 // TODO: ADD YOUR CODE BELOW
+let age = prompt("Enter your age");
+age = parseInt(age);
+
+
+if (age >= 18) {
+  let registered = confirm("Are you registered to vote?");
+  if (registered) {
+    console.log("Great! You are all set to vote.");
+  }else{
+    console.log("You still have time to register to vote.");
+  }
+}else{
+  let yearsLeft = 18 - age;
+  console.log(`You will be eligible to vote in ${yearsLeft} years`);
+}
+
 
 /*************************************************************************************************
-Task 5 (if..else Statement): 
+Task 5 (if..else Statement):
 
 Description:
   You are building a pizza ordering system.
@@ -96,7 +144,7 @@ Steps:
     - Use an if-else statement to check the length of the toppings array and calculate the toppingCost accordingly.
     - If the length of the toppings array is less than or equal to 2, multiply the length by 2 to get the toppingCost.
     - If the length of the toppings array is greater than 2, multiply the length by 2 and then multiply by 0.9 to get the toppingCost   .
-  
+
   Step 5: Ask the user if they would like to add a drink
     - Use the prompt() method to ask the user if they want to add a drink to their order.
     - Use the toLowerCase() method to convert the user input to lowercase for easier comparison.
@@ -106,19 +154,63 @@ Steps:
     - Create a variable called drinkCost.
     - Use an if statement to check if the user wants to add a drink.
     - If the user wants to add a drink, set the drinkCost variable to 2.
- 
+
   Step 7: Calculate the total cost of the order
     - Create a variable called totalCost.
     - Add the price, toppingCost, and drinkCost variables together to get the totalCost.
- 
+
   Step 8: Apply discounts based on the user's order
     - Use if-else-if statements to check if the user qualifies for any discounts.
     - If the user adds more than 2 toppings and orders a drink, apply a 15% discount to the totalCost.
     - If the user adds more than 2 toppings, apply a 10% discount to the totalCost.
     - If the user orders a drink, subtract $2 from the totalCost.
- 
+
   Step 9: Display the total cost of the order to the user
     - Use the console.log() method to display the totalCost variable to the user in the form of a string.
     - Use the toFixed() method to format the totalCost to 2 decimal places.
 *************************************************************************************************/
 // TODO: ADD YOUR CODE BELOW
+let size = prompt("Enter the pizza size (small, medium, or large)");
+size = size.toLowerCase();
+
+let price;
+if (size == "small") {
+  price = 10;
+}else if (size == "medium") {
+  price = 15;
+}else if (size == "large") {
+  price = 20;
+}else{
+  console.log("Invalid pizza size");
+}
+
+
+let toppings = prompt("Enter the toppings you want, separated by commas");
+toppings = toppings.split(",");
+let toppingCost;
+if (toppings.length <= 2) {
+  toppingCost = toppings.length * 2;
+}else{
+  toppingCost = toppings.length * 2 * 0.9;
+}
+
+
+let drink = prompt("Would you like to add a drink to your order? (yes or no)");
+drink = drink.toLowerCase();
+let drinkCost;
+if (drink == "yes") {
+  drinkCost = 2;
+}
+
+
+let totalCost = price + toppingCost + drinkCost;
+if (toppings.length > 2 && drink == "yes") {
+  totalCost *= 0.85;
+}else if (toppings.length > 2) {
+  totalCost *= 0.9;
+}else if (drink == "yes") {
+  totalCost -= 2;
+}
+
+
+console.log(`The total cost of your order is $${totalCost.toFixed(2)}`);
