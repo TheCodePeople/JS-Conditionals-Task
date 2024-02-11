0c638c7112572cac820c0e74934c093a2747e192
ZhraaBaharAluloom <zahraambaharaluloom@gmail.com>
2024-02-11 09:45:17 +0300

    Rewrite some of the tasks instructionin the switch statment file

--- switchStatement.js
+++ switchStatement.js
@@ -10,20 +10,21 @@ Example:
   - Your horoscope for the day: You may feel like you are in a rut today. Try doing something out of your comfort zone to break the monotony.
 
 Steps:
-  1. Create a variable sign and prompt the user to enter their zodiac sign.
-  2. Create a switch statement that checks the value of sign.
+  1. Create a variable named `zodiacSign` and assign its value to a zodiac sign of your choice.
+  2. Create a switch statement that checks the value of the sign.
     - For each case, write a horoscope message for that sign for the day.
     - If the user enters a sign that is not recognized by the switch statement, display an error message saying "Sorry, we do not have a horoscope for that sign."
-Note: Check the file called "Clothing_Recommendations.md" to find the list of weather forecasts and their corresponding clothing recommendations  
+
+*Note: Check the file called "Zodiac_Signs.md" to find the list of horoscope messages.*  
 ************************************************************************************************/
-// TODO: ADD YOUR CODE BELOW
+//  TODO: ADD YOUR CODE BELOW
 
 /************************************************************************************************
 Task 6 (Switch Statement):
     Create a program that helps you decide what to wear based on the weather forecast for the day.
 
 Steps:
-  1. Create a variable weather and prompt the user to enter the forecasted weather for the day (e.g. sunny, cloudy, rainy, snowy).
+  1. Create a variable named `weather` and assign its value to forecasted weather for the day (e.g. sunny, cloudy, rainy, snowy).
   2. Create a switch statement that checks the value of weather.
     - For each case, write a message recommending appropriate clothing for that weather condition.
     - If the user enters a weather condition that is not recognized by the switch statement, display an error message saying "Sorry, we do not have recommendations for that weather condition."
@@ -34,5 +35,7 @@ Example:
   ------------------------------------ 
   - What's the weather forecast for today? Sunny
   - Recommendations: Wear sunscreen, a hat, and sunglasses.
+
+*Note: Check the file called "Clothing_Recommendations.md" to find the list of weather forecasts and their corresponding clothing recommendations.*  
 ************************************************************************************************/
-// TODO: ADD YOUR CODE BELOW
+//  TODO: ADD YOUR CODE BELOW
